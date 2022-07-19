640802b5b4db3b4336e833b9c845a72212cf54f5
Dave Paroulek <david.paroulek@camunda.com>
2022-07-19 16:02:46 -0400

    Fixed compilation errors

---
+++ src/test/java/org/camunda/community/extension/zeebe/exporter/jobworker/EmbeddedJobWorkerTest.java
@@ -0,0 +1,13 @@
+package org.camunda.community.extension.zeebe.exporter.jobworker;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class EmbeddedJobWorkerTest {
+
+  @Test
+  public void sanityTest() {
+    assertTrue(true);
+  }
+}
